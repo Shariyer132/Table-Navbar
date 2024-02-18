602b6c40a8548dbcc1bcada23761aa3da43cb7ac
shariyer132 <shariyer132@gmail.com>
2024-02-18 23:43:48 +0600

    admin link updated

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -8,12 +8,17 @@ const Dashboard = () => {
             <Navbar />
             <div className="flex gap-2">
                 <div className="w-1/5 pt-3">
-                    <h4 className="bg-gray-300 pl-7 text-base py-2 w-full font-medium">Admin Links</h4>
-                    
-                        <Link className="flex hover:bg-[#D0CBC8] py-2 pl-7 font-semibold items-center gap-1 " ><TbCategoryPlus /><span className="hidden sm:block">Admin Links</span></Link>
-                        <Link className="flex hover:bg-[#D0CBC8] py-2 pl-7 font-semibold items-center gap-1 " ><TbCategoryPlus /><span className="hidden sm:block">Admin Links</span></Link>
-                        <Link className="flex hover:bg-[#D0CBC8] py-2 pl-7 font-semibold items-center gap-1 " ><TbCategoryPlus /><span className="hidden sm:block">Admin Links</span></Link>
-                 
+                    <h4 className="bg-gray-300 md:pl-7 pl-2 text-base py-2 w-full font-medium">Admin Links</h4>
+                    <div className="hidden sm:block">
+                        <Link className="flex hover:bg-[#D0CBC8] py-2 md:pl-7 sm:pl-2 pl-5 font-semibold items-center gap-1" ><TbCategoryPlus />Admin Links</Link>
+                        <Link className="flex hover:bg-[#D0CBC8] py-2 md:pl-7 sm:pl-2 pl-5 font-semibold items-center gap-1 " ><TbCategoryPlus />Admin Links</Link>
+                        <Link className="flex hover:bg-[#D0CBC8] py-2 md:pl-7 sm:pl-2 pl-5 font-semibold items-center gap-1 " ><TbCategoryPlus />Admin Links</Link>
+                    </div>
+                 <div className="sm:hidden">
+                 <Link className="flex hover:bg-[#D0CBC8] py-2 pl-7 font-semibold items-center gap-1 tooltip tooltip-right" data-tip="Admin Links" ><TbCategoryPlus /></Link>
+                 <Link className="flex hover:bg-[#D0CBC8] py-2 pl-7 font-semibold items-center gap-1 tooltip tooltip-right" data-tip="Admin Links" ><TbCategoryPlus /></Link>
+                 <Link className="flex hover:bg-[#D0CBC8] py-2 pl-7 font-semibold items-center gap-1 tooltip tooltip-right" data-tip="Admin Links" ><TbCategoryPlus /></Link>
+                 </div>
                 </div>
                 <div className="w-4/5"><Outlet /></div>
             </div>
